71ad97d88ad8e06698996a9c543540d20d74f5d0
Thomas <thomas.chaudru@gmail.com>
2022-11-07 11:54:27 +0100

    password verification added

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
 const mailVerif = require('../middleware/mail-verif');
+const passwordVerif = require('../middleware/password-verif');
 
-router.post('/signup', mailVerif, userCtrl.signup);
+router.post('/signup', mailVerif, passwordVerif, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
